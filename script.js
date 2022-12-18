e7f8d6d447b43c78efc359f8ef082d2fd400f796
miwanoff <marina.iva.noffa@gmail.com>
2022-12-18 12:54:40 +0200

    Add mini

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let timer;
 let slider = document.getElementById("slider");
 let nextButton = document.getElementById("next");
 let startButton = document.getElementById("start");
+let miniatures = document.getElementsByClassName("mini");
 
 function next() {
   if (i >= arrSrc.length - 1) {
@@ -31,5 +32,14 @@ function start() {
   }, 3000);
 }
 
+function showSlide(event) {
+  let imageMini = event.target;
+  slider.src = imageMini.src;
+}
+
 nextButton.addEventListener("click", next);
 startButton.addEventListener("click", start);
+
+for (let j = 0; j < miniatures.length; j++) {
+  miniatures[j].addEventListener("click", showSlide);
+}
