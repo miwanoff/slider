fc4c35d4c6ec87d45bfe5b8f59936c5d65b4d555
miwanoff <marina.iva.noffa@gmail.com>
2022-12-18 12:16:20 +0200

    initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let arrSrc = [
+  "images/landscape-photography-tips-yosemite-valley-feature.webp",
+  "images/lanscape.jfif",
+  "images/UK_advice-gardening-growing-poppies_header.jpg",
+]; // масив шляхів до картинок
+let i = 0;
+let slider = document.getElementById("slider");
